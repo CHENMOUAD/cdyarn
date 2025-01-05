fcd8818019084a47ddd592554dec974a7744021f
CHENMOUAD <mnbvcqwe009@gmail.com>
2025-01-05 16:32:20 +0800

    Update agent.js

--- js/agent.js
+++ js/agent.js
@@ -1,12 +1,1 @@
-(function () {
-  fetch(`https://agent.worker.xiangyueye.com/agent/add-relation?agentId=4325270`, 
-    {
-      method: 'GET',
-      mode: 'no-cors',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    })
-  .then(response => response.json())
-  .then(data => console.log(data))    // 处理返回的数据
-})()
+
